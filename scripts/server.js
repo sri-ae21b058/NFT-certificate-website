6f37e01d1541159bba866c2013f162875eb7f81f
Srihari S <ae21b058@smail.iitm.ac.in>
2023-02-03 20:05:37 +0530

    API creation

--- scripts/server.js
+++ scripts/server.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const app=express();
+const deploycontract = require('./deploy.js');
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
+    console.log(deploycontract.message);
 });
